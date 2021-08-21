bc71fd01b097495f71117e423f4542d01f26a456
Vinicius Perroni <vinicius.perroni@outlook.com>
2021-08-21 16:18:27 -0300

    Ajustes no layout

--- Infolog/src/Infolog.Web/Controllers/HomeController.cs
+++ Infolog/src/Infolog.Web/Controllers/HomeController.cs
@@ -1,38 +1,36 @@
-﻿using Infolog.Web.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Infolog.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
         public HomeController(ILogger<HomeController> logger)
         {
-            //Teste
-            _logger = logger;
         }
 
+        #region Telas
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        #endregion
+
+        #region Ações
+        public IActionResult Salvar()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Deletar()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return RedirectToAction("Index");
         }
+        #endregion
     }
 }
